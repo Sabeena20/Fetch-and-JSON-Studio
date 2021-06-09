3b05764ab8f8b2e35525b8f24ae23561d3946779
Sabeena <sabeenarayyan13@gmail.com>
2021-06-08 21:57:53 -0500

    modified code

--- script.js
+++ script.js
@@ -6,16 +6,20 @@ window.addEventListener("load",function(){
         response.json().then( function(json){
             // console.log(json);
             const image = document.getElementsByClassName("avatar");
-            const div = document.querySelector("bio");
+            const div = document.getElementById("container");
             // const div = document.getElementById("container");
             for(let i = 0;i < json.length;i++){
                 div.innerHTML = `
+                <div class = "astronaut" >
+                <div class = "bio">
                 <h3> ${json[i].firstName} ${json[i].lastName} </h3>
                 <ul>
                 <li> Hours in space:${json[i].hoursInSpace} </li>
                 <li> Skills:${json[i].skills} </li>
                 <li> Active:${json[i].active} </li>
+                </div>
                 <img src = ${json[i].picture} </img>
+                </div>
                 `;
                 // image.src = `${json[i].picture}`;
                 // console.log(json[i].id);
